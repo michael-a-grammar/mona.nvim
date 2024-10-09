18f90ac17a0d7c00ab2cbf067852becbe1ca0ddb
michael-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2024-10-09 20:11:22 +0100

    Updates

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -1,25 +1,25 @@
 vim.opt.rtp:append(".")
 
 for _, repository in ipairs({
-  "nvim-lua/plenary.nvim",
-  "nvim-telescope/telescope.nvim",
+   "nvim-lua/plenary.nvim",
+   "nvim-telescope/telescope.nvim",
 }) do
-  local _, directory = string.match(repository, "(.*)/(.*)")
+   local _, directory = string.match(repository, "(.*)/(.*)")
 
-  directory = "/tmp/" .. directory
+   directory = "/tmp/" .. directory
 
-  local is_directory = vim.fn.isdirectory(directory) ~= 0
+   local is_directory = vim.fn.isdirectory(directory) ~= 0
 
-  if not is_directory then
-    vim.fn.system({
-      "git",
-      "clone",
-      "https://github.com/" .. repository,
-      directory,
-    })
-  end
+   if not is_directory then
+      vim.fn.system({
+         "git",
+         "clone",
+         "https://github.com/" .. repository,
+         directory,
+      })
+   end
 
-  vim.opt.rtp:append(directory)
+   vim.opt.rtp:append(directory)
 end
 
 vim.cmd("runtime plugin/plenary.vim")
